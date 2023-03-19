d83ed1273a6631ce6cc4c56940eb8a849329971b
Your Name <arun@Penguin.local>
2023-03-19 14:28:51 +0100

    Minor change

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -4,26 +4,30 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
-import org.springframework.data.annotation.Id;
+import lombok.RequiredArgsConstructor;
 
-@AllArgsConstructor
 @Getter
+@RequiredArgsConstructor
 public class User {
     private String id;
 
     @JsonProperty("first_name")
+    @NotNull
     @NotEmpty(message = "First Name is required")
     private String firstName;
 
     @JsonProperty("last_name")
+    @NotNull
     @NotEmpty(message = "Last Name is required")
     private String lastName;
 
     @Min(value = 18, message = "Age cannot be less than 18")
+    @NotNull
     private int age;
 
     @Email(message = "Please provide a valid email")
+    @NotNull
     private String email;
 }
